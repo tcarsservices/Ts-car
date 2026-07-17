639c9dcd672bc76892aa3ef6180188aee65b7e88
tcarsservices <45kdzy22hn@privaterelay.appleid.com>
2026-07-18 01:38:16 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+const observer = new IntersectionObserver(
+  (entries) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add("show");
+      }
+    });
+  },
+  {
+    threshold: 0.12
+  }
+);
+
+document.querySelectorAll(".reveal").forEach((element) => {
+  observer.observe(element);
+});
